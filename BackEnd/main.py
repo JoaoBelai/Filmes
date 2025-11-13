e6dc022685ec827481ee86ef1697508fd9d51ba5
Joao Belai <joaopbelai@gmail.com>
2025-11-12 21:32:44 -0300

    arrumando cors do_options

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -12,7 +12,7 @@ class MyHandle(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
         self.end_headers()
         self.wfile.write(json.dumps(dados).encode('utf-8'))
 
@@ -21,7 +21,7 @@ class MyHandle(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
         self.end_headers()
 
 
